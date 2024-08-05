9cf9f47a6d883c1aa7122c02d2b914996f9bddc8
StelSdou <stelsd05@gmail.com>
2024-08-05 03:06:05 +0300

    ok

--- upload.php
+++ upload.php
@@ -9,23 +9,33 @@ if ($_POST["submit"]){
     if (!move_uploaded_file($pdf_file['tmp_name'], $upload_file)) { 
         die("Possible file upload attack!");
     }
-    // echo $upload_file;
+    echo $upload_file;
 
     try {
         $imagick = new Imagick();
 
         $imagick->readImage($upload_file);
-
+        
         foreach ($imagick as $i => $page) {
             $page->setImageFormat('jpg');
             $imageBlob = $page->getImageBlob();
-            
-            $stmt = $conn->prepare("INSERT INTO october (NAME, yearD, num, image) VALUES (?, ?, ?, ?)");
-            // $stmt->bind_param("ib", $i, $imageBlob);
-            $stmt->bind_param("ssib", $_POST['name'], $_POST['Year'], $i, $imageBlob);
-            $stmt->send_long_data(3, $imageBlob);
+            $imagePath = $upload_dir . "page_" . $i . "_" . time() . ".jpg";
+            file_put_contents($imagePath, $imageBlob);
+
+            $stmt = $conn->prepare("INSERT INTO october (NAME, yearD, num, image_path) VALUES (?, ?, ?, ?)");
+            $stmt->bind_param("ssis", $_POST['name'], $_POST['Year'], $i, $imagePath);
             $stmt->execute();
-        }    
+        }
+        // foreach ($imagick as $i => $page) {
+        //     $page->setImageFormat('jpg');
+        //     $imageBlob = $page->getImageBlob();
+        //     $imageType = 'image/jpeg';
+    
+        //     $stmt = $conn->prepare("INSERT INTO october (NAME, yearD, num, image, image_type) VALUES (?, ?, ?, ?, ?)");
+        //     $stmt->bind_param("ssiss", $_POST['name'], $_POST['Year'], $i, $imageBlob, $imageType);
+        //     $stmt->send_long_data(3, $imageBlob);
+        //     $stmt->execute();
+        // }    
     } catch (Exception $e) {
         echo 'Caught exception: <br>',  $e->getMessage(), "\n";
     }
